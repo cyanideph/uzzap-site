4d2d5198ab759fd283e666eb3b5549c8dbf8dc41
cyanideph <67945955+cyanideph@users.noreply.github.com>
2026-09-20 09:55:53 +0800

    chore(sentry): configure website DSN

--- sentry-config.js
+++ sentry-config.js
@@ -1,3 +1,2 @@
 // Public Sentry DSN for the Uzzap website. DSNs are client identifiers, not API auth tokens.
-// Set this after creating the Uzzap website Sentry project.
-window.UZZAP_SENTRY_DSN = "";
+window.UZZAP_SENTRY_DSN = "https://97304e7e8e0f9bd92650baac72f750b3@o4507866800717824.ingest.us.sentry.io/4512116166361088";
